3e9565c9ab7af3fbaf84fe9823826658edec32b6
SohaIqbal <145153061+SohaIqbal@users.noreply.github.com>
2023-09-24 15:07:28 +0500

    Add files via upload

---
+++ landing page/j.js
@@ -0,0 +1,16 @@
+setTimeout(function() {
+    const delayedText = document.querySelector('.text');
+    delayedText.classList.add('appear');
+  }, 1500); // 1000 milliseconds = 1 second
+  
+
+  setTimeout(function() {
+    const delayedText = document.querySelector('.nav h5');
+    delayedText.classList.add('appear');
+  }, 1000); // 1000 milliseconds = 1 second
+
+
+  setTimeout(function() {
+    const delayedText = document.querySelector('.nav img');
+    delayedText.classList.add('appear');
+  }, 1000);
