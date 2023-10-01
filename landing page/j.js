e04bab2c25bd42243c3c6f9d30917b766ff884e9
SohaIqbal <145153061+SohaIqbal@users.noreply.github.com>
2023-10-01 16:44:27 +0500

    j.js

--- landing page/j.js
+++ landing page/j.js
@@ -14,3 +14,27 @@ setTimeout(function() {
     const delayedText = document.querySelector('.nav img');
     delayedText.classList.add('appear');
   }, 1000);
+
+var main=document.querySelector(".main");
+var c=document.querySelector("#cursor");
+
+main.addEventListener("mousemove",function(dets){
+    c.style.left=dets.x+"px";
+    c.style.top=dets.y+"px";
+})
+
+var sc=document.querySelector(".secondsection");
+var c=document.querySelector("#cursor");
+
+sc.addEventListener("mousemove",function(dets){
+    c.style.left=dets.x+"px";
+    c.style.top=dets.y+"px";
+})
+
+var contact=document.querySelector(".contact");
+var c=document.querySelector("#cursor");
+
+contact.addEventListener("mousemove",function(dets){
+    c.style.left=dets.x+"px";
+    c.style.top=dets.y+"px";
+})
