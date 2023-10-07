f21ec3e2f384ccf36c8f5bf874e6c5a7bf6d419f
SohaIqbal <145153061+SohaIqbal@users.noreply.github.com>
2023-10-08 00:32:17 +0500

    j.js

--- calculator/j.js
+++ calculator/j.js
@@ -8,3 +8,77 @@ cont.addEventListener("mousemove",function(dets){
     c.style.left=dets.x+"px";
     c.style.top=dets.y+"px";
 })
+
+
+const display = document.getElementById('result');
+const buttons = document.querySelectorAll('.calculator input[type="button"]');
+
+
+let currentInput = '';
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => buttonClick(button));
+});
+
+function buttonClick(button) {
+    const buttonValue = button.value;
+
+    
+    if (!isNaN(buttonValue) || buttonValue === '.') {
+        currentInput += buttonValue;
+        updateDisplay();
+    }
+
+
+    else if (isOperator(buttonValue)) {
+        currentInput += buttonValue;
+        updateDisplay();
+    }
+
+   
+    else if (buttonValue === '=') {
+        try {
+            currentInput = eval(currentInput).toString();
+            updateDisplay();
+        } catch (error) {
+            currentInput = 'Error';
+            updateDisplay();
+        }
+    }
+
+  
+    else if (buttonValue === 'AC') {
+        clear();
+    }
+
+   
+    else if (buttonValue === 'DE') {
+        currentInput = currentInput.slice(0, -1);
+        updateDisplay();
+    }
+}
+function updateDisplay() {
+    display.value = currentInput;
+}
+
+function isOperator(value) {
+    return ['+', '-', '*', '/'].includes(value);
+}
+
+function clear() {
+    currentInput = '';
+    updateDisplay();
+}
+function isOperator(value) {
+    return ['+', '-', '*', '/'].includes(value);
+}
+function calculate() {
+}
+function clear() {
+    currentInput = '';
+    updateDisplay();
+}
+function deleteLastCharacter() {
+    
+}
